fc1c971cb6f657d7fd8a17b71acc9e4837981fd1
Default Person <kblaha@daptiv.com>
2013-10-11 15:12:43 -0700

    Update personal settings

--- modules/people/manifests/kblaha.pp
+++ modules/people/manifests/kblaha.pp
@@ -1,9 +1,13 @@
-# Replace name of all the "kblaha" with your github username
-# if you have a dash (-) in your username use an underscore (_) instead
 class people::kblaha {
   
-  #install windows remote desktop
   include daptiv::rdp
+
+  include daptiv::fishShell
+  include iterm2::dev
+
+  include daptiv::sublime3
+
+  include chrome
 
   #add projects chefclient, ppm, ppmspa and dev dashboard
   include projects::ppm
@@ -16,14 +20,11 @@ class people::kblaha {
     value  => 'kblaha@daptiv.com'
   }
   git::config::global { 'user.name':
-    value  => 'Default Person'
+    value  => 'Karel Blaha'
   }
 
-  #link in your personal dot files the provided files live in the people/files dir and 
-  #you should copy them to a folder matching your personal user if you intend to personalize them 
-  #if you do not copy these your dotfiles will change when this kblaha profile is updated as they
-  #are symlinked into your home directory.
   $home = "/Users/${::boxen_user}"
+
   file { "${home}/.bash_profile":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/kblaha/.bash_profile"
