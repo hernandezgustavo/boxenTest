30d97d94dd94815223f0ccf26c386d224620e75f
Chris Bobo <chrisbobo@gmail.com>
2013-10-15 12:28:51 -0700

    Add osx customizations

--- manifests/site.pp
+++ manifests/site.pp
@@ -76,35 +76,24 @@ node default {
 
   #ensure that boxen environment is set up upon shell start
   #/etc/profile is loaded by all login shell starts
-  file { 
-    '/etc/profile': 
-    ensure => present 
+  file {
+    '/etc/profile':
+    ensure => present
   }
 
-  file_line { 'source_boxen': 
-    line => 'source /opt/boxen/env.sh', 
-    path => '/etc/profile', 
+  file_line { 'source_boxen':
+    line => 'source /opt/boxen/env.sh',
+    path => '/etc/profile',
   }
 
-  file_line { 'increase_ulimit': 
-    line => 'ulimit -n 3000', 
-    path => '/etc/profile', 
+  file_line { 'increase_ulimit':
+    line => 'ulimit -n 3000',
+    path => '/etc/profile',
   }
 
-  file_line { 'localhost_daptiv_com': 
-    line => '127.0.0.1 localhost.daptiv.com', 
-    path => '/etc/hosts', 
-  }
-
-  exec { 'killall Finder':
-    refreshonly => true
-  }
-  boxen::osx_defaults { 'show hidden files':
-    user   => $::boxen_user,
-    key    => 'AppleShowAllFiles',
-    domain => 'com.apple.Finder',
-    value  => true,
-    notify => Exec["killall Finder"]
+  file_line { 'localhost_daptiv_com':
+    line => '127.0.0.1 localhost.daptiv.com',
+    path => '/etc/hosts',
   }
 
   include daptiv::dotFiles
