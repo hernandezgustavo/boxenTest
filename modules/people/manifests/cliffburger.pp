3dc84594ce3fde4973d43a0fa1ca0a1bcc112a91
Cliff Burger <cliff.burger@gmail.com>
2013-10-17 13:40:47 -0700

    correct file location

--- modules/people/manifests/cliffburger.pp
+++ modules/people/manifests/cliffburger.pp
@@ -10,29 +10,29 @@ class people::cliffburger.pp {
 
   #add personal git configurations
   git::config::global { 'user.email':
-    value  => 'cliffburger.pp@daptiv.com'
+    value  => 'cliffburger@daptiv.com'
   }
   git::config::global { 'user.name':
-    value  => 'Default Person'
+    value  => 'Cliff Burger'
   }
 
   #link in your personal dot files the provided files live in the people/files dir and
   #you should copy them to a folder matching your personal user if you intend to personalize them
-  #if you do not copy these your dotfiles will change when this cliffburger.pp profile is updated as they
+  #if you do not copy these your dotfiles will change when this cliffburger profile is updated as they
   #are symlinked into your home directory.
   $home = "/Users/${::boxen_user}"
   file { "${home}/.bash_profile":
     ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger.pp/.bash_profile"
+    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger/.bash_profile"
   }
 
   file { "${home}/.git-completion.sh":
     ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger.pp/git-completion.sh"
+    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger/git-completion.sh"
   }
 
   file { "${home}/.git-prompt.sh":
     ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger.pp/git-prompt.sh"
+    target  => "${$boxen::config::repodir}/modules/people/files/cliffburger/git-prompt.sh"
   }
 }
