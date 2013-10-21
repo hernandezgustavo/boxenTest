e304f0ece42d04c7d3a13af02b338b7520c1fc18
Chris Bobo <chrisbobo@gmail.com>
2013-10-21 11:24:49 -0700

    Update chrisbobo.pp

--- modules/people/manifests/chrisbobo.pp
+++ modules/people/manifests/chrisbobo.pp
@@ -1,7 +1,7 @@
 class people::chrisbobo {
   $home = "/Users/${::boxen_user}"
 
-  include dropbox
+#  include dropbox
 #  include daptiv::apps::googledrive
 #  include daptiv::apps::vmware
 
@@ -15,15 +15,15 @@ class people::chrisbobo {
     target  => "${$boxen::config::repodir}/modules/people/files/chrisbobo/.bash_profile"
   }
 
-  file { "${home}/.git-completion.sh":
-    ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/shared/git-completion.sh"
-  }
+#  file { "${home}/.git-completion.sh":
+#    ensure  => link,
+#    target  => "${$boxen::config::repodir}/modules/people/files/shared/git-completion.sh"
+#  }
 
-  file { "${home}/.git-prompt.sh":
-    ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/shared/git-prompt.sh"
-  }
+#  file { "${home}/.git-prompt.sh":
+#    ensure  => link,
+#    target  => "${$boxen::config::repodir}/modules/people/files/shared/git-prompt.sh"
+#  }
 
 
   #------------------------
@@ -54,71 +54,71 @@ class people::chrisbobo {
   #------------------------
   # Sublime Customizations
   #------------------------
-  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings":
-    ensure  => link,
-    target  => "${home}/src/sublimeconfig/Preferences.sublime-settings"
-  }
+#  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings":
+#    ensure  => link,
+#    target  => "${home}/src/sublimeconfig/Preferences.sublime-settings"
+#  }
 
-  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings":
-    ensure  => link,
-    target  => "${home}/src/sublimeconfig/SublimeLinter.sublime-settings"
-  }
+#  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings":
+#    ensure  => link,
+#    target  => "${home}/src/sublimeconfig/SublimeLinter.sublime-settings"
+#  }
 
-  sublime_text_3::package { 'Wombat Theme':
-    source => 'git@github.com:sheerun/sublime-wombat-theme'
-  }
+#  sublime_text_3::package { 'Wombat Theme':
+#    source => 'git@github.com:sheerun/sublime-wombat-theme'
+#  }
 
-  sublime_text_3::package { 'BracketHighlighter':
-    source => 'git@github.com:facelessuser/BracketHighlighter'
-  }
+#  sublime_text_3::package { 'BracketHighlighter':
+#    source => 'git@github.com:facelessuser/BracketHighlighter'
+#  }
 
-  sublime_text_3::package { 'sublime-jsdocs':
-    source => 'git@github.com:spadgos/sublime-jsdocs'
-  }
+#  sublime_text_3::package { 'sublime-jsdocs':
+#    source => 'git@github.com:spadgos/sublime-jsdocs'
+#  }
 
-  sublime_text_3::package { 'sublime-grunt':
-    source => 'git@github.com:tvooo/sublime-grunt'
-  }
+#  sublime_text_3::package { 'sublime-grunt':
+#    source => 'git@github.com:tvooo/sublime-grunt'
+#  }
 
-  repository{
-    'my sublime config':
-      source   => 'git@github.com:chrisbobo/sublimeconfig', #short hand for github repos
-      provider => 'git',
-      path => "${home}/src/sublimeconfig",
-      force => true
-  }
+#  repository{
+#    'my sublime config':
+#      source   => 'git@github.com:chrisbobo/sublimeconfig', #short hand for github repos
+#      provider => 'git',
+#      path => "${home}/src/sublimeconfig",
+#      force => true
+#  }
 
 
   #------------------------
   # Osx Customizations
   #------------------------
-  include daptiv::osx::dialogs::expand_print_dialog
-  include daptiv::osx::dialogs::expand_save_dialog
+#  include daptiv::osx::dialogs::expand_print_dialog
+#  include daptiv::osx::dialogs::expand_save_dialog
 
-  include daptiv::osx::system::disable_app_quarantine
+#  include daptiv::osx::system::disable_app_quarantine
 
-  include daptiv::osx::dock::autohide
-  include daptiv::osx::dock::orientation
-  class { 'daptiv::osx::dock::icon_size':
-    size => 64
-  }
+#  include daptiv::osx::dock::autohide
+#  include daptiv::osx::dock::orientation
+#  class { 'daptiv::osx::dock::icon_size':
+#    size => 64
+#  }
 
-  include daptiv::osx::finder::show_all_on_desktop
-  include daptiv::osx::finder::show_hidden_files
-  include daptiv::osx::finder::unhide_library
+#  include daptiv::osx::finder::show_all_on_desktop
+#  include daptiv::osx::finder::show_hidden_files
+#  include daptiv::osx::finder::unhide_library
 
-  include daptiv::osx::keyboard::enable_keyboard_control_access
-  include daptiv::osx::keyboard::use_standard_function_keys
+#  include daptiv::osx::keyboard::enable_keyboard_control_access
+#  include daptiv::osx::keyboard::use_standard_function_keys
 
-  include daptiv::osx::system::menu::date_format
-  include daptiv::osx::system::menu::show_battery_percentage
+#  include daptiv::osx::system::menu::date_format
+#  include daptiv::osx::system::menu::show_battery_percentage
 
-  include daptiv::osx::mouse::enable_right_click
+#  include daptiv::osx::mouse::enable_right_click
 
-  include daptiv::osx::no_network_dsstores
+#  include daptiv::osx::no_network_dsstores
 
-  include daptiv::osx::terminal::default_directory
-  include daptiv::osx::terminal::profile
+#  include daptiv::osx::terminal::default_directory
+#  include daptiv::osx::terminal::profile
 
 }
 
