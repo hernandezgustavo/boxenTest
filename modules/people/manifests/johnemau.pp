c0e43090edb5c62d6b92f699d2bc287d12dfd81b
John Emau <jemau@daptiv.com>
2013-12-10 11:52:46 -0800

    updated sublime support in personal manifest

--- modules/people/manifests/johnemau.pp
+++ modules/people/manifests/johnemau.pp
@@ -3,8 +3,10 @@ class people::johnemau {
   include iterm2::dev
   include macvim
   include sublime_text_3::package_control
+
   include apps::vim::typescript
   include apps::fishShell
+  include apps::git::aliases
 
   include projects::devdashboard
   include projects::chefclient
@@ -12,9 +14,15 @@ class people::johnemau {
   include projects::ppmspa
 
   $home = "/Users/${::boxen_user}"
+  $files = "${$boxen::config::repodir}/modules/people/files/johnemau"
 
   file { "${home}/.bash_profile":
-    ensure  => "${$boxen::config::repodir}/modules/people/files/johnemau/.bash_profile"
+    ensure  => "${files}/.bash_profile"
+  }
+
+  file { "${home}/.config/fish/personal.fish":
+    ensure  => "${files}/personal.fish",
+    subscribe => File["${home}/.config/fish/"]
   }
 
   # fixes warning if using vim syntastic plugin with fish shell
@@ -27,6 +35,20 @@ class people::johnemau {
   file_line { 'load_vim_plugins_via_pathogen':
     line => 'execute pathogen#infect()',
     path => "${vim::vimrc}"
+  }
+
+  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings":
+    ensure  => "${files}/Preferences.sublime-settings"
+  }
+
+  sublime_text_3::package { 'Floobits':
+    source => 'git@github.com:Floobits/floobits-sublime'
+  }
+  sublime_text_3::package { 'Wombat Theme':
+    source => 'git@github.com:sheerun/sublime-wombat-theme'
+  }
+  sublime_text_3::package { 'BracketHighlighter':
+    source => 'git@github.com:facelessuser/BracketHighlighter'
   }
 
   git::config::global { 'user.email':
