c6c9e856f0d2cda42a5d06bbe1b4726ac1e17426
Chris Bobo <chrisbobo@gmail.com>
2014-01-17 10:46:11 -0800

    home var missing from git completion and prompt manifests

--- modules/apps/manifests/git/completion.pp
+++ modules/apps/manifests/git/completion.pp
@@ -1,4 +1,6 @@
 class apps::git::completion {
+  $home = "/Users/${::boxen_user}"
+
   file { "${home}/.git-completion.sh":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/shared/git-completion.sh"
--- modules/apps/manifests/git/prompt.pp
+++ modules/apps/manifests/git/prompt.pp
@@ -1,4 +1,6 @@
 class apps::git::prompt {
+  $home = "/Users/${::boxen_user}"
+
   file { "${home}/.git-prompt.sh":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/shared/git-prompt.sh"
