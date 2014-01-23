4166ba08a03687eb9d7401f437589d80c1613464
Chris Bobo <chrisbobo@gmail.com>
2014-01-23 12:55:54 -0800

    Update aliases.pp

--- modules/apps/manifests/git/aliases.pp
+++ modules/apps/manifests/git/aliases.pp
@@ -6,14 +6,14 @@ class apps::git::aliases {
 
     git::config::global { 'alias.logg':
       value  => 'log --graph'
-    } ->
+    }
 
     git::config::global { 'alias.logsl':
       value  => 'log --oneline --decorate'
-    } ->
+    }
 
     git::config::global { 'alias.logfg':
       value  => 'log --pretty=oneline --abbrev-commit --graph --decorate --all'
-    } ->
+    }
 
 }
