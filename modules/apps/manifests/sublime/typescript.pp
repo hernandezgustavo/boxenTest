5410790b29a82ea76f380519de6cef55b58ab78c
Jonathan Park <jpark@daptiv.com>
2014-02-10 17:21:03 -0800

    Updating to sublime linter 3

--- modules/apps/manifests/sublime/typescript.pp
+++ modules/apps/manifests/sublime/typescript.pp
@@ -1,5 +1,9 @@
 class apps::sublime::typescript {
   sublime_text_3::package { 'T3S':
-    source => 'git@github.com:Railk/T3S'
+    source => 'git@github.com:Railk/T3S',
+    branch => 'dev'
+  }
+  sublime_text_3::package { 'SublimeLinter_colors':
+    source => 'git@github.com:ericclemmons/sublime-typescript.git'
   }
 }
