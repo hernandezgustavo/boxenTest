d3b4913f953ab66dd3b7fae79c8abb6fe9f0b110
Jonathan Park <jpark@daptiv.com>
2014-02-25 08:39:07 -0800

    fix history

---
+++ modules/apps/manifests/sublime/markdown.pp
@@ -0,0 +1,8 @@
+class apps::sublime::markdown {
+  sublime_text_3::package { 'sublime-markdown-extended':
+    source => 'git@github.com:jonschlinkert/sublime-markdown-extended'
+  }
+  sublime_text_3::package { 'sublime-monokai-extended':
+    source => 'git@github.com:jonschlinkert/sublime-monokai-extended'
+  }
+}
