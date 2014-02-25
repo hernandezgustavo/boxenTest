29ba8ae6c4a1a0d71b4bfbe985aae9d5e34e5eb8
Jonathan Park <jpark@daptiv.com>
2014-02-25 09:33:46 -0800

    Adding markdown toc generation

--- modules/apps/manifests/sublime/markdown.pp
+++ modules/apps/manifests/sublime/markdown.pp
@@ -5,4 +5,7 @@ class apps::sublime::markdown {
   sublime_text_3::package { 'sublime-monokai-extended':
     source => 'git@github.com:jonschlinkert/sublime-monokai-extended'
   }
+  sublime_text_3::package { 'MarkdownTOC':
+    source => 'git@github.com:naokazuterada/MarkdownTOC.git'
+  }
 }
