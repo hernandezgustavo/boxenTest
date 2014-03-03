84b0c87d5615028f951fbce3ed7917f24ac0fd85
greglboxer <gboxer@daptiv.com>
2014-03-03 15:12:10 -0800

    customized personal files

--- modules/people/manifests/greglboxer.pp
+++ modules/people/manifests/greglboxer.pp
@@ -6,12 +6,13 @@ class people::greglboxer {
   # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
   # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
   # Update the path below to point to that file and uncomment this section.
-  #vagrant::plugin { 'vagrant-vmware-fusion':
-  #  license => '${$boxen::config::repodir}/modules/people/files/greglboxer/LICENSE_FILENAME.lic'
-  #}
+  
+  vagrant::plugin { 'vagrant-vmware-fusion':
+    license => "${$boxen::config::repodir}/modules/people/files/greglboxer/VagrantVMWareFusionLicense_gboxer.lic"
+  }
 
   git::config::global { 'user.email':
-    value  => 'GITHUB_EMAIL'
+    value  => 'gboxer@daptiv.com'
   }
   git::config::global { 'user.name':
     value  => 'greglboxer'
@@ -25,4 +26,51 @@ class people::greglboxer {
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/greglboxer/.bash_profile"
   }
+
+  include apps::googledrive
+  include apps::fishShell
+
+  include apps::sublime
+  include apps::sublime::bracket_highlighter
+  include apps::sublime::wombat_theme
+  include apps::sublime::jsdocs
+  include apps::sublime::grunt
+  include apps::sublime::git
+  include apps::sublime::history
+  include apps::sublime::markdown
+  include apps::sublime::fileautocomplete
+  include apps::sublime::sidebar
+  include sublime_text_3::package_control
+
+  nodejs::module { 'typescript-tools':
+    node_version => 'v0.10',
+    ensure => '0.2.2-1'
+  }
+  
+  #daptiv
+  #---------------------------------------------------
+  include projects::ppm
+  include projects::chefclient
+  include projects::ppmspa
+  include projects::devdashboard
+  #---------------------------------------------------
+
+  #osx
+  #---------------------------------------------------
+  include osx::disable_app_quarantine
+  include osx::no_network_dsstores
+
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+
+  #include osx::keyboard::use_standard_function_keys
+
+  include osx::finder::show_all_on_desktop
+  include osx::finder::show_hidden_files
+  include osx::finder::unhide_library
+  include osx::finder::enable_quicklook_text_selection
+
+  include osx::dock::dim_hidden_apps
+  #---------------------------------------------------
 }
