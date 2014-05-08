5aef482935baf86c17d41b5c87e361d00965a77b
Adam Kalman <akalman@daptiv.com>
2014-05-08 16:44:36 -0700

    add personalization to npmrc file

--- modules/daptiv/manifests/npm/registry.pp
+++ modules/daptiv/manifests/npm/registry.pp
@@ -1,8 +1,11 @@
 class daptiv::npm::registry {
   $home = "/Users/${::boxen_user}"
 
-  file { "${home}/.npmrc":
-    ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/daptiv/files/npm/.npmrc"
+  file { "npmrc":
+    path => "${home}/.npmrc",
+    ensure => file,
+    replace => false,
+    mode => "0644",
+    source  => "${$boxen::config::repodir}/modules/daptiv/files/npm/.npmrc"
   }
 }
