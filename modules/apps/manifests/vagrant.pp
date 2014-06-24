c67992a3dc4046fc01f9bf5fbe7340842e234f6d
Jonathan Park <jpark@daptiv.com>
2014-06-24 15:12:44 -0700

    Remove vagrant plugin uninstall

--- modules/apps/manifests/vagrant.pp
+++ modules/apps/manifests/vagrant.pp
@@ -1,16 +1,6 @@
 class apps::vagrant {
   include vmware_fusion
+
   include vagrant
-
-  vagrant::plugin { 'vagrant-berkshelf':
-    ensure => 'absent',
-    before => Class['vagrant']
-  }
-
-  #vagrant windows is depricated and should no longer be used.
-  vagrant::plugin { 'vagrant-windows':
-    ensure => 'absent',
-    before => Class['vagrant']
-  }
 
 }
