4165d2bb5939aac2eba78731217300bb5f2b1fa4
Greg Boxer <gboxer@daptiv.com>
2014-06-25 15:23:10 -0700

    Update greglboxer.pp
    Switched to zsh from fish

--- modules/people/manifests/greglboxer.pp
+++ modules/people/manifests/greglboxer.pp
@@ -3,14 +3,14 @@
 class people::greglboxer {
   $home = "/Users/${::boxen_user}"
 
-  # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
-  # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
-  # Update the path below to point to that file and uncomment this section.
-  
   vagrant::plugin { 'vagrant-vmware-fusion':
     license => "${$boxen::config::repodir}/modules/people/files/greglboxer/VagrantVMWareFusionLicense_gboxer.lic"
   }
 
+  include iterm2::dev
+  include chrome::canary
+  include zsh
+  
   git::config::global { 'user.email':
     value  => 'gboxer@daptiv.com'
   }
@@ -18,17 +18,12 @@ class people::greglboxer {
     value  => 'greglboxer'
   }
 
-  # link in your personal dot files the provided files live in the people/files dir and
-  # you should copy them to a folder matching your personal user if you intend to personalize them
-  # if you do not copy these your dotfiles will change when this greglboxer profile is updated as they
-  # are symlinked into your home directory.
   file { "${home}/.bash_profile":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/greglboxer/.bash_profile"
   }
 
   include apps::googledrive
-  include apps::fishShell
 
   include apps::sublime
   include apps::sublime::bracket_highlighter
