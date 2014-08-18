c408adfc868f2eb1bd1df7c65f11c84180b7bb02
Neil Breeden III <nbreeden@daptiv.com>
2014-08-18 14:44:49 -0700

    Adding personal config

---
+++ modules/people/manifests/neiliux.pp
@@ -0,0 +1,28 @@
+# Replace name of all the "neiliux" with your github username
+# if you have a dash (-) in your username use an underscore (_) instead
+class people::neiliux {
+  $home = "/Users/${::boxen_user}"
+
+  # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
+  # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
+  # Update the path below to point to that file and uncomment this section.
+  #vagrant::plugin { 'vagrant-vmware-fusion':
+  #  license => '${$boxen::config::repodir}/modules/people/files/neiliux/LICENSE_FILENAME.lic'
+  #}
+
+  git::config::global { 'user.email':
+    value  => 'nbreeden@daptiv.com'
+  }
+  git::config::global { 'user.name':
+    value  => 'Neil Breeden III'
+  }
+
+  # link in your personal dot files the provided files live in the people/files dir and
+  # you should copy them to a folder matching your personal user if you intend to personalize them
+  # if you do not copy these your dotfiles will change when this neiliux profile is updated as they
+  # are symlinked into your home directory.
+  file { "${home}/.bash_profile":
+    ensure  => link,
+    target  => "${$boxen::config::repodir}/modules/people/files/neiliux/.bash_profile"
+  }
+}
