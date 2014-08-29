e61301428b48fc3e45a54d55e5449dcd2186a9b6
Matthew Potter <mpotter@daptiv.com>
2014-08-29 09:38:04 -0700

    Updating personal files

--- modules/people/manifests/dachew.pp
+++ modules/people/manifests/dachew.pp
@@ -1,44 +1,11 @@
-# Replace name of all the "dachew" with your github username
-# if you have a dash (-) in your username use an underscore (_) instead
 class people::dachew {
 
   $home = "/Users/${::boxen_user}"
-
-  # link in your personal dot files the provided files live in the people/files dir and
-  # you should copy them to a folder matching your personal user if you intend to personalize them
-  # if you do not copy these your dotfiles will change when this drewburlingame profile is updated as they
-  # are symlinked into your home directory.
-  file { "${home}/.bash_profile":
-    ensure  => link,
-    target  => "${$boxen::config::repodir}/modules/people/files/dachew/.bash_profile"
-  }
-
-  #add projects chefclient, ppm, ppmspa and dev dashboard
-  include dropbox
-  include apps::googledrive
-  include projects::ppm
-  include projects::chefclient
-  include projects::ppmspa
-  include projects::devdashboard
-  include projects::devscripts
-
-  vagrant::plugin { 'vmware-fusion':
-    license => "${boxen::config::repodir}/modules/people/files/dachew/VagrantVMWareFusionLicense-mpotter.lic"
-  }
-
-  #add personal git configurations
-  git::config::global { 'user.email':
-    value  => 'matthew@synapseware.com'
-  }
-  git::config::global { 'user.name':
-    value  => 'Matthew Potter'
-  }
 
   #link in your personal dot files the provided files live in the people/files dir and
   #you should copy them to a folder matching your personal user if you intend to personalize them
   #if you do not copy these your dotfiles will change when this dachew profile is updated as they
   #are symlinked into your home directory.
-  $home = "/Users/${::boxen_user}"
   file { "${home}/.bash_profile":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/dachew/.bash_profile"
@@ -54,6 +21,29 @@ class people::dachew {
     target  => "${$boxen::config::repodir}/modules/people/files/dachew/git-prompt.sh"
   }
 
+
+  #add projects chefclient, ppm, ppmspa and dev dashboard
+  include apps::googledrive
+  include apps::copy
+  include apps::sublime
+  include apps::sublime::bracket_highlighter
+  include projects::ppm
+  include projects::chefclient
+  include projects::ppmspa
+  include projects::devdashboard
+
+  vagrant::plugin { 'vmware-fusion':
+    license => "${boxen::config::repodir}/modules/people/files/dachew/VagrantVMWareFusionLicense-mpotter.lic"
+  }
+
+  #add personal git configurations
+  git::config::global { 'user.email':
+    value  => 'matthew@synapseware.com'
+  }
+  git::config::global { 'user.name':
+    value  => 'Matthew Potter'
+  }
+
   #------------------------
   # Osx Customizations
   #------------------------
@@ -66,6 +56,5 @@ class people::dachew {
   include osx::global::expand_save_dialog
 
   include osx::finder::show_all_on_desktop
-  include osx::finder::show_hidden_files
   include osx::finder::unhide_library
 }
