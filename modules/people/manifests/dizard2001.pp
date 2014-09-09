0c6e2088b02b3bfb8a2c9efcdcb6a4de0a26df23
dizard2001 <darrel.anderson@changepoint.com>
2014-09-09 10:41:57 -0700

    dizard2001 personal files

---
+++ modules/people/manifests/dizard2001.pp
@@ -0,0 +1,28 @@
+# Replace name of all the "dizard2001" with your github username
+# if you have a dash (-) in your username use an underscore (_) instead
+class people::dizard2001 {
+  $home = "/Users/${::boxen_user}"
+
+  # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
+  # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
+  # Update the path below to point to that file and uncomment this section.
+  #vagrant::plugin { 'vagrant-vmware-fusion':
+  #  license => "${$boxen::config::repodir}/modules/people/files/dizard2001/LICENSE_FILENAME.lic"
+  #}
+
+  git::config::global { 'user.email':
+    value  => 'GITHUB_EMAIL'
+  }
+  git::config::global { 'user.name':
+    value  => 'dizard2001'
+  }
+
+  # link in your personal dot files the provided files live in the people/files dir and
+  # you should copy them to a folder matching your personal user if you intend to personalize them
+  # if you do not copy these your dotfiles will change when this dizard2001 profile is updated as they
+  # are symlinked into your home directory.
+  file { "${home}/.bash_profile":
+    ensure  => link,
+    target  => "${$boxen::config::repodir}/modules/people/files/dizard2001/.bash_profile"
+  }
+}
