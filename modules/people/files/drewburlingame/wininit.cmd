340d6a1608e2c7c81551d6fce92e77a2caead70e
drewburlingame <drewburlingame@gmail.com>
2014-09-12 14:49:44 -0700

    working wininit.cmd

--- modules/people/files/drewburlingame/wininit.cmd
+++ modules/people/files/drewburlingame/wininit.cmd
@@ -1,12 +1,11 @@
 rem #-- let powershell execute scripts
 powershell -Command "Set-ExecutionPolicy Unrestricted -Force"
-SET chocolatey_bin_root=c:\chocolatey\bin
+SET chocolatey_bin_root=C:\ProgramData\chocolatey\stuff
 
 rem #-- install chocolatey
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-SET PATH=%PATH%;%chocolatey_bin_root%
+SET PATH=%PATH%;%chocolatey_bin_root%;C:\ProgramData\chocolatey\bin;
 
-rem #-- install boxstarter
 cinst boxstarter
 
-powershell -ExecutionPolicy unrestricted -Command "Install-BoxstarterPackage -PackageName C:\hosthome\src\setup-windows\changepoint\setup.ps1"
+powershell -ExecutionPolicy unrestricted -File "C:\hosthome\src\setup-windows\changepoint\setup.ps1"
