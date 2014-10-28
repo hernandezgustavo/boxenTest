e2ac65bee57b3715d3ec88fe5af4581d49281894
Sam Noedel <sam.noedel@changepoint.com>
2014-10-28 09:04:33 -0700

    Adding Sam's config files

--- modules/people/files/delta62/.bash_profile
+++ modules/people/files/delta62/.bash_profile
@@ -6,15 +6,31 @@ export PATH=~/bin:$PATH
 source ~/.git-completion.sh
 source ~/.git-prompt.sh
 
-# Git shorthand
-alias gs="git status"
-alias gd="git diff"
-alias gc="git commit"
-alias gpl="git pull"
-alias gp="git push"
-alias ga="git add"
-alias gl="git log"
-
+# General aliases
+alias l="ls"
 alias ls="ls -lA"
+alias grep="grep -i --colour"
+
+export DB_SERVER_NAME="WIN-2008R2SP1"
 
 PS1='$(printf "%$((`tput cols`-1))s\r")$(stat -f "\e[1;32m%Sp \e[0;33m%Su:%Sg\e[0;39m" .)$(__git_ps1 " \e[0;94m%s\e[0;37m")\n\w: '
+
+# Git bindings
+
+delta62_git_alias() {
+    alias $2="$1"
+    if [ "$3" ]
+    then
+        __git_complete $2 $3
+    fi
+}
+
+delta62_git_alias "git status"   gs  
+delta62_git_alias "git branch"   gb   _git_branch
+delta62_git_alias "git diff"     gd   _git_diff
+delta62_git_alias "git commit"   gc   _git_commit
+delta62_git_alias "git pull"     gpl  _git_pull
+delta62_git_alias "git push"     gp   _git_push
+delta62_git_alias "git add"      ga   _git_add
+delta62_git_alias "git log"      gl   _git_log
+delta62_git_alias "git checkout" gco  _git_checkout
--- modules/people/manifests/delta62.pp
+++ modules/people/manifests/delta62.pp
@@ -1,8 +1,12 @@
 class people::delta62 {
   $home = "/Users/${::boxen_user}"
 
+  # Programs
+  include firefox
   include chrome::canary
   include iterm2::dev
+  include screen
+  include apps::webstorm
 
   # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
   # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
@@ -13,18 +17,20 @@ class people::delta62 {
   }
 
   git::config::global { 'user.email':
-    value  => 'sam@samnoedel.com'
+    value  => 'sam.noedel@changepoint.com'
   }
   git::config::global { 'user.name':
     value  => 'Sam Noedel'
   }
 
-  # link in your personal dot files the provided files live in the people/files dir and
-  # you should copy them to a folder matching your personal user if you intend to personalize them
-  # if you do not copy these your dotfiles will change when this default profile is updated as they
-  # are symlinked into your home directory.
+  # Dotfiles
   file { "${home}/.bash_profile":
     ensure  => link,
     target  => "${$boxen::config::repodir}/modules/people/files/delta62/.bash_profile"
+  }
+
+  file { "${home}/.screenrc":
+    ensure => link,
+    target => "${boxen::config::repodir}/modules/people/files/delta62/.screenrc"
   }
 }
