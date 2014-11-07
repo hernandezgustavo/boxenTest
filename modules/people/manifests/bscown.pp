3c013d3179a4de164462a80830a88d9ac47b354a
Bryan Scown <bryan.scown@changepoint.com>
2014-11-07 09:15:20 -0800

    updated vagrant license

--- modules/people/manifests/bscown.pp
+++ modules/people/manifests/bscown.pp
@@ -1,7 +1,23 @@
 # Replace name of all the "bscown" with your github username
 # if you have a dash (-) in your username use an underscore (_) instead
 class people::bscown {
-  include  virtualbox
+  include gitflow
+  include flowdock
+  include homebrewcask
+  include bettertouchtool
+  include heroku
+
+  $vmware_license = "${$boxen::config::repodir}/modules/people/files/${$::github_login}/VagrantVMWareFusionLicense.lic"
+
+  #if you have a license file in your user dir install vmware-fusion vagrant plugin
+  vagrant::plugin { 'vagrant-vmware-fusion':
+    license => $vmware_license
+  }
+ 
+  exec {  "set_vmware_fusion_key_bscown":
+    command=> "'/Applications/VMware Fusion.app/Contents/Library/vmware-licenseTool' enter 4N2AK-2H1EN-J8L62-08CU2-9N3NM '' '' '6.0' 'VMware Fusion for Mac OS' ''",
+    user => root
+  }
 
   #add personal git configurations
   git::config::global { 'user.email':
