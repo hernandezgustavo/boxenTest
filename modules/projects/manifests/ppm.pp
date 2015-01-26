81eab8d9456ae9d91dd428299f93538730532e0d
James Trinklein <james.trinklein@changepoint.com>
2015-01-26 12:27:45 -0800

    Remove chef-repo clone, as it is not required anymore

--- modules/projects/manifests/ppm.pp
+++ modules/projects/manifests/ppm.pp
@@ -6,8 +6,4 @@ class projects::ppm {
     line => '192.168.56.101 devppm.daptiv.com devsso.daptiv.com devapi.daptiv.com devadminapi.daptiv.com devsso.daptiv.com',
     path => '/etc/hosts'
   }
-
-  boxen::project { 'chef-repo':
-    source        => 'git@github.com:daptiv/chef-repo'
-  }
 }
