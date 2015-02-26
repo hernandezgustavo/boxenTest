3f0fac4837cf798a87c312290ad0879db370c6a5
Neil Breeden III <neil.breeden@changepoint.com>
2015-02-26 15:10:44 -0800

    Adding shell aliases and functions.

--- modules/people/files/neiliux/personal.fish
+++ modules/people/files/neiliux/personal.fish
@@ -1,2 +1,43 @@
 alias p=pico
+alias "gitbranchclean"="git branch -D (git branch | trim)"
+
+function g
+    git $argv
+end
+
+function gs
+    git status $argv
+end
+
+function gp
+    git pull $argv
+end
+
+function gpu
+    git push $argv
+end
+
+function gc
+    git commit $argv
+end
+
+function gr
+    grunt $argv
+end
+
+function gcb
+    git checkout -b $argv
+end
+
+function gcm
+    git checkout master
+end
+
+function gbpn
+    grunt build-ci --production=true --no-optimize
+end
+
+function gbp
+   grunt build-ci --production=true
+end
 
