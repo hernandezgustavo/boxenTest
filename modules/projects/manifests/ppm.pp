77a0c384f0f3b8430841755e3063c56cffa90588
Jonathan Park <jonathan.park@changepoint.com>
2015-03-18 13:04:43 -0700

    Add host configuration for vm to hiera

--- modules/projects/manifests/ppm.pp
+++ modules/projects/manifests/ppm.pp
@@ -1,9 +1,4 @@
 class projects::ppm {
   include projects::chefclient
   include apps::vagrant
-
-  file_line { 'ppm_hosts':
-    line => '192.168.56.101 devppm.daptiv.com devsso.daptiv.com devapi.daptiv.com devadminapi.daptiv.com devsso.daptiv.com',
-    path => '/etc/hosts'
-  }
 }
