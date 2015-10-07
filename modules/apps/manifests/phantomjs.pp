651d41952d603772cb1d7793854ec7da1e5b4100
Shawn Neal <sneal@sneal.net>
2015-10-07 11:06:18 -0700

    Bump PhantomJS to 1.9.8
    - 1.9.0 package failed to installed because it doesn't exist in HomeBrew, at least on El Capitan.

--- modules/apps/manifests/phantomjs.pp
+++ modules/apps/manifests/phantomjs.pp
@@ -2,6 +2,6 @@ class apps::phantomjs {
     # Installs Phantomenv for PhantomJS version management
     include phantomjs
 
-    # Install PhantomJS version 1.9.0
-    phantomjs::version { '1.9.0': }
+    # Install PhantomJS version 1.9.8
+    phantomjs::version { '1.9.8': }
 }
