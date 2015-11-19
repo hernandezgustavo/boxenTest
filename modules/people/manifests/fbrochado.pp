735dec4ac50da73bcef36ad109bab31a8ec1c8f7
fbrochado <GITHUB_EMAIL>
2015-11-19 16:09:06 -0300

    Adding vm ware fusion license to manifest

--- modules/people/manifests/fbrochado.pp
+++ modules/people/manifests/fbrochado.pp
@@ -6,9 +6,9 @@ class people::fbrochado {
   # To automatically have the vagrant vmware windows plugin license entered for you, do the following:
   # Upload your vagrant vmware plugin license to /modules/people/files/<your github username>
   # Update the path below to point to that file and uncomment this section.
-  #vagrant::plugin { 'vagrant-vmware-fusion':
-  #  license => "${$boxen::config::repodir}/modules/people/files/fbrochado/LICENSE_FILENAME.lic"
-  #}
+  vagrant::plugin { 'vagrant-vmware-fusion':
+    license => "${$boxen::config::repodir}/modules/people/files/fbrochado/LICENSE-2-vagrant-vmware-provider-license_20_seats.lic.lic"
+  }
 
   git::config::global { 'user.email':
     value  => 'GITHUB_EMAIL'
