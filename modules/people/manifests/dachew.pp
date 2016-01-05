de8c7e8e4be1ce4ef11d7d6d2291a94b90d1c0e0
Ramun McCallum <matthew@synapseware.com>
2016-01-05 11:28:56 -0800

    Update dachew.pp

--- modules/people/manifests/dachew.pp
+++ modules/people/manifests/dachew.pp
@@ -4,17 +4,17 @@ class people::dachew {
   $cfg = "${home}/src/configuration/changepoint"
   $vmare_key = "M069M-6HL82-M8L63-0898H-803KN"
 
-  include apps::sublime
-  include apps::sublime::bracket_highlighter
-  include projects::ppm
-  include projects::devdashboard
-  include git
-  include apps::git::aliases
-  include apps::git::completion
-  include chrome
-  include firefox
-  include vmware_fusion
-  include flowdock
+  #include apps::sublime
+  #include apps::sublime::bracket_highlighter
+  #include projects::ppm
+  #include projects::devdashboard
+  #include git
+  #include apps::git::aliases
+  #include apps::git::completion
+  #include chrome
+  #include firefox
+  #include vmware_fusion
+  #include flowdock
 
 
   #-------------------------------------------------------------
@@ -71,68 +71,68 @@ class people::dachew {
 
   # Decrypt license file using OpenSSL
   # http://osxdaily.com/2012/01/30/encrypt-and-decrypt-files-with-openssl/
-  exec { "decrypt_subl_license":
-    command => "openssl des3 -d -in license-info.enc -out license-info -pass pass:6VS+AMYmg6",
-    path => "${cfg}/licenses/"
-  }
+  #exec { "decrypt_subl_license":
+  #  command => "openssl des3 -d -in license-info.enc -out license-info -pass pass:6VS+AMYmg6",
+  #  path => "${cfg}/licenses/"
+  #}
 
   # Sublime Text - license
-  file { "${home}/Library/Application Support/Sublime Text 3/Local/License.sublime_license":
-    ensure => file,
-    target => "${cfg}/licenses/license-info"
-  }
+  #file { "${home}/Library/Application Support/Sublime Text 3/Local/License.sublime_license":
+  #  ensure => file,
+  #  target => "${cfg}/licenses/license-info"
+  #}
 
   # Create a symlink for starting Sublime Text from the terminal
-  file { '/usr/local/bin/subl':
-    ensure  => link,
-    target  => '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
-  }
+  #file { '/usr/local/bin/subl':
+  #  ensure  => link,
+  #  target  => '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
+  #}
 
 
   #-------------------------------------------------------------
   # VIM syntax for Puppet
   # VIM syntax location for mac OS: /usr/share/vim/vim73/
   #-------------------------------------------------------------
-  repository { 'puppet-syntax-vim':
-	source   => 'git@github.com:puppetlabs/puppet-syntax-vim.git',
-	provider => 'git',
-	path     => "${home}/src/puppet-syntax-vim/",
-	force    => true
-  }
-  file { '/usr/share/vim/vim73/ftdetect':
-  	source	=> "${home}/src/puppet-syntax-vim/ftdetect",
-	recurse	=> true
-  }
-  file { '/usr/share/vim/vim73/ftplugin':
-  	source	=> "${home}/src/puppet-syntax-vim/ftplugin",
-	recurse	=> true
-  }
-  file { '/usr/share/vim/vim73/indent':
-  	source	=> "${home}/src/puppet-syntax-vim/indent",
-	recurse	=> true
-  }
-  file { '/usr/share/vim/vim73/syntax':
-  	source	=> "${home}/src/puppet-syntax-vim/syntax",
-	recurse	=> true
-  }
+  #repository { 'puppet-syntax-vim':
+#	source   => 'git@github.com:puppetlabs/puppet-syntax-vim.git',
+#	provider => 'git',
+#	path     => "${home}/src/puppet-syntax-vim/",
+#	force    => true
+ # }
+  #file { '/usr/share/vim/vim73/ftdetect':
+  #	source	=> "${home}/src/puppet-syntax-vim/ftdetect",
+#	recurse	=> true
+ # }
+  #file { '/usr/share/vim/vim73/ftplugin':
+  #	source	=> "${home}/src/puppet-syntax-vim/ftplugin",
+#	recurse	=> true
+ # }
+  #file { '/usr/share/vim/vim73/indent':
+  #	source	=> "${home}/src/puppet-syntax-vim/indent",
+#	recurse	=> true
+ # }
+  #file { '/usr/share/vim/vim73/syntax':
+  #	source	=> "${home}/src/puppet-syntax-vim/syntax",
+#	recurse	=> true
+ # }
 
 
 
   #-------------------------------------------------------------
   # Osx Customizations
   #-------------------------------------------------------------
-  file { "${home}/Library/KeyBindings/DefaultKeyBinding.dict":
-	ensure	=> link,
-	target	=> "${cfg}/changepoint/macos/DefaultKeyBinding.dict"
-  }
+  #file { "${home}/Library/KeyBindings/DefaultKeyBinding.dict":
+#	ensure	=> link,
+#	target	=> "${cfg}/changepoint/macos/DefaultKeyBinding.dict"
+#  }
 
-  include osx::disable_app_quarantine
-  include osx::no_network_dsstores
+#  include osx::disable_app_quarantine
+#  include osx::no_network_dsstores
 
-  include osx::global::enable_keyboard_control_access
-  include osx::global::expand_print_dialog
-  include osx::global::expand_save_dialog
+#  include osx::global::enable_keyboard_control_access
+#  include osx::global::expand_print_dialog
+#  include osx::global::expand_save_dialog
 
-  include osx::finder::show_all_on_desktop
-  include osx::finder::unhide_library
+#  include osx::finder::show_all_on_desktop
+#  include osx::finder::unhide_library
 }
