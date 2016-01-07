533c28dab6b61228e27c9f0fd635c3581f5e82a9
Ramun McCallum <ramun.mccallum@changepoint.com>
2016-01-07 09:41:33 -0800

    More cool vim customizations

--- modules/people/manifests/dachew.pp
+++ modules/people/manifests/dachew.pp
@@ -4,30 +4,19 @@ class people::dachew {
   $cfg = "${home}/src/configuration"
   $vmare_key = "M069M-6HL82-M8L63-0898H-803KN"
 
-  include apps::vagrant
-  include apps::vmware_fusion
-  include apps::nodejs
 
 
   #-------------------------------------------------------------
   # Special User Configurations
   #-------------------------------------------------------------
   repository { 'configuration':
-	source   => 'git@github.com:dachew/configuration', #short hand for github repos
-	provider => 'git',
-	path     => "${cfg}"
+  	source   => 'git@github.com:dachew/configuration', #short hand for github repos
+	  provider => 'git',
+  	path     => "${cfg}"
   }
   file { "${home}/.bash_profile":
     ensure	=> link,
     target	=> "${cfg}/changepoint/macos/.bash_profile"
-  }
-  file { "${home}/.vimrc":
-    ensure	=> link,
-    target	=> "${cfg}/changepoint/macos/.vimrc"
-  }
-  file { "${home}/.vim":
-    ensure	=> link,
-    target	=> "${cfg}/changepoint/macos/.vim/"
   }
   file { "${home}/.gitconfig":
     ensure	=> link,
@@ -38,18 +27,19 @@ class people::dachew {
     target	=> "${cfg}/changepoint/macos/.gitattributes"
   }
 
+
   #-------------------------------------------------------------
   # License VMWare Fusion  
   #-------------------------------------------------------------
-  vagrant::plugin { 'vagrant-vmware-fusion':
-    license => "${boxen::config::repodir}/modules/people/files/dachew/VagrantVMWareFusionLicense-rmccallum.lic"
-  }
   exec { "license_vmware_fusion":
-    command=> "vmware-licenseTool enter ${vmware_key} '' '' '7.0' 'VMware Fusion for Mac OS' ''",
-    path => '/Applications/VMware Fusion.app/Contents/Library',
+    command=> "vmware-licenseTool enter ${vmware_key} '' '' '7.1.2' 'VMware Fusion for Mac OS' ''",
+    path => '/Applications/VMware Fusion.app/Contents/Library/licenses',
     user => root,
     refreshonly => true,
     subscribe => Package['VMware Fusion']
+  }
+  vagrant::plugin { 'vagrant-vmware-fusion':
+    license => "${boxen::config::repodir}/modules/people/files/dachew/VagrantVMWareFusionLicense-rmccallum.lic"
   }
 
 
@@ -67,11 +57,11 @@ class people::dachew {
   # symlink the correct user folder
   exec { "remove-default-user-folder":
     command => "rm -rf \"${st}/Packages/User\"",
-	onlyif => "test -d \"${st}/Packages/User\""
+    onlyif => "test -d \"${st}/Packages/User\""
   }
   exec { "fix-subl-user-folder":
     command => "ln -Fs \"${cfg}/changepoint/macos/sublime-text\" \"${st}/Packages/User\"",
-	onlyif => "test ! -L \"${st}/Packages/User\""
+    onlyif => "test ! -L \"${st}/Packages/User\""
   }
 
   # Decrypt license file using OpenSSL
@@ -89,43 +79,71 @@ class people::dachew {
 
 
   #-------------------------------------------------------------
-  # VIM syntax for Puppet
-  # VIM syntax location for mac OS: /usr/share/vim/vim73/
+  # VIM
   #-------------------------------------------------------------
+  file { "${home}/.vimrc":
+    ensure	=> link,
+    target	=> "${cfg}/changepoint/macos/.vimrc"
+  }
+  file { "${home}/.vim":
+    ensure	=> directory 
+  }
+  file { "${home}/.vim/autoload":
+    ensure  => directory
+  }
+  file { "${home}/.vim/bundle":
+    ensure  => directory
+  }
+  exec { "install-pathogen":
+    command => "curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim",
+    onlyif  => "test ! -f ~/.vim/autoload/pathogen.vim"
+  }
   repository { 'puppet-syntax-vim':
-	source   => 'git@github.com:rodjek/vim-puppet.git',
-	provider => 'git',
-	path     => "${home}/src/vim-puppet/",
-	force    => true
+    source  => 'git@github.com:rodjek/vim-puppet.git',
+    provider=> 'git',
+    path    => "${home}/.vim/bundle/vim-puppet/"
   }
-  file { '/usr/share/vim/vim73/ftdetect':
-    source	=> "${home}/src/vim-puppet/ftdetect",
-    recurse	=> true
+  repository { 'vim-go':
+    source  => 'git@github.com:fatih/vim-go.git',
+    provider=> 'git',
+    path    => "${home}/.vim/bundle/vim-go/"
   }
-  file { '/usr/share/vim/vim73/ftplugin':
-    source	=> "${home}/src/vim-puppet/ftplugin",
-    recurse	=> true
+  repository { 'vim-javascript':
+    source  => 'git@github.com:jelera/vim-javascript-syntax',
+    provider=> 'git',
+    path    => "${home}/.vim/bundle/vim-javascript-syntax/"
   }
-  file { '/usr/share/vim/vim73/indent':
-    source	=> "${home}/src/vim-puppet/indent",
-    recurse	=> true
-  }
-  file { '/usr/share/vim/vim73/syntax':
-    source	=> "${home}/src/vim-puppet/syntax",
-    recurse	=> true
+  repository { 'vim-airline':
+    source  => 'git@github.com:bling/vim-airline',
+    provider=> 'git',
+    path    => "${home}/.vim/bundle/vim-airline/"
   }
 
+
+  #-------------------------------------------------------------
+  # Install golang 
+  #-------------------------------------------------------------
+  package { "golang":
+    ensure   => installed,
+    provider => 'pkgdmg',
+    source   => "https://storage.googleapis.com/golang/go1.5.1.darwin-amd64.pkg",
+  }
 
 
   #-------------------------------------------------------------
   # Osx Customizations
   #-------------------------------------------------------------
   file { "${home}/Library/KeyBindings/DefaultKeyBinding.dict":
-	ensure	=> link,
-	target	=> "${cfg}/changepoint/macos/DefaultKeyBinding.dict"
+	  ensure	=> link,
+	  target	=> "${cfg}/changepoint/macos/DefaultKeyBinding.dict"
   }
 
+  include apps::vagrant
+  include apps::vmware_fusion
+  include apps::nodejs
   include osx::no_network_dsstores
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
+  include osx::finder::show_hidden_files
+  include osx::finder::unhide_library
 }
