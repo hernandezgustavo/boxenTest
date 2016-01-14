cc4c2e49e44bccea2734c2bc720ca2c19eff7196
Neil Breeden III <neil.breeden@changepoint.com>
2016-01-14 14:09:30 -0800

    Adding console2 settings files file and updating wininit scripts.

--- modules/people/files/neiliux/wininit.cmd
+++ modules/people/files/neiliux/wininit.cmd
@@ -1,6 +1,6 @@
 SET BOXEN_SCRIPTS=c:\src\our-boxen\modules\people\files\%1
 IF EXIST "%BOXEN_SCRIPTS%" (
-	setx.exe BOXEN_SCRIPTS "c:\src\our-boxen-modules\people\files\%1"
+	setx.exe BOXEN_SCRIPTS "c:\src\our-boxen\modules\people\files\%1"
 )
 
 powershell.exe -Command "Set-ExecutionPolicy Unrestricted -Force"
--- modules/people/files/neiliux/wininit.ps1
+++ modules/people/files/neiliux/wininit.ps1
@@ -5,4 +5,15 @@ $env:PATH += $env:ALLUSERSPROFILE + "\chocolatey\bin"
 Write-Host "Installing Apps"
 choco install console2
 
+Write-Host "Updating configuration"
+$consoleSettingsFolder = $env:USERPROFILE + "\AppData\Roaming\Console\"
+$consoleSettingsFilePath = $consoleSettingsFolder + "\console.xml"
+$consoleSourceSettingsFilePath = $env:BOXEN_SCRIPTS + "\console.xml"
+
+Write-Host $consoleSettingsFilePath
+Write-Host $env:BOXEN_SCRIPTS
+
+new-item $consoleSettingsFolder -type directory -errorAction SilentlyContinue
+copy-item $consoleSourceSettingsFilePath $consoleSettingsFilePath -force
+
 Write-Host "Done!"
