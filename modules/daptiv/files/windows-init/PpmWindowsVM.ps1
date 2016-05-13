081f4e5f1b673c2e249eaceb43f8088f5ee67abc
Adam Kalman <akalman@users.noreply.github.com>
2016-05-13 15:41:29 -0700

    have windows setup script also setup auth server

--- modules/daptiv/files/windows-init/PpmWindowsVM.ps1
+++ modules/daptiv/files/windows-init/PpmWindowsVM.ps1
@@ -1,3 +1,13 @@
+gem install bundle
+
+#setup auth
+git clone git@github.com:daptiv/Daptiv.Services.Auth C:/src/Daptiv.Services.Auth
+cd C:/src/Daptiv.Services.Auth
+bundle install
+bundle exec rake
+chef-client -j oic.json
+
+#setup ppm
 git clone git@github.com:daptiv/Ppm C:/src/Ppm
 cd C:/src/Ppm
 git submodule init
@@ -5,5 +15,6 @@ git submodule update
 bundle install
 bundle exec rake
 npm install -g grunt-cli
-chef-client
+chef-client -r "daptiv_dev_ppm_role::deploy_ppm,minitest-handler"
+
 chef-client -o beyondcompare
