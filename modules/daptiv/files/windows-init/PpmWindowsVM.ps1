92f1b97bf39d66b9b05d9e39be3d424385d4696b
Adam Kalman <akalman@daptiv.com>
2016-05-18 10:54:39 -0700

    reorder installs and do appropriate cleanup
    auth tests are dependent on EPS, which is created by ppm
    bootstrapping process, so we have to load ppm first.  This causes
    us to have to run chef on ppm twice.  There may be a way around
    this (maybe run chef with -o and -r?).

--- modules/daptiv/files/windows-init/PpmWindowsVM.ps1
+++ modules/daptiv/files/windows-init/PpmWindowsVM.ps1
@@ -1,12 +1,5 @@
 gem install bundle
 
-#setup auth
-git clone git@github.com:daptiv/Daptiv.Services.Auth C:/src/Daptiv.Services.Auth
-cd C:/src/Daptiv.Services.Auth
-bundle install
-bundle exec rake
-chef-client -j oic.json
-
 #setup ppm
 git clone git@github.com:daptiv/Ppm C:/src/Ppm
 cd C:/src/Ppm
@@ -14,7 +7,19 @@ git submodule init
 git submodule update
 bundle install
 bundle exec rake
-npm install -g grunt-cli
+npm install -g grunt-cli mountebank
+chef-client -r "daptiv_dev_ppm_role::deploy_ppm,minitest-handler"
+
+#setup auth
+git clone git@github.com:daptiv/Daptiv.Services.Auth C:/src/Daptiv.Services.Auth
+cd C:/src/Daptiv.Services.Auth
+bundle install
+bundle exec rake
+chef-client -j oic.json
+
+#reset default chef run list
+#see [TO BE FILLED IN]
+cd C:/src/Ppm
 chef-client -r "daptiv_dev_ppm_role::deploy_ppm,minitest-handler"
 
 chef-client -o beyondcompare
