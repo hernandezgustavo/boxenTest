2e01ed53d46ac3714b3b644fe72edc7003519b17
Adam Kalman <akalman@daptiv.com>
2016-05-24 13:00:08 -0700

    clean up unused files

--- modules/people/files/cliffburger/personal.fish
+++
@@ -1,6 +0,0 @@
-alias subl='subl3'
-set -x EDITOR 'subl3 -w'
-alias mountSrc='hdiutil mount ~/Documents/sourceFolder.sparsebundle/ -mountpoint ~/src'
-alias ls='ls -al'
-alias startvm2="vmrun -T fusion start \"$VM2_PATH\" nogui"
-alias stopvm2="vmrun -T fusion stop \"$VM2_PATH\""
