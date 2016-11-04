324b197fcac7773a6f85248829784792c7c965a6
James Trinklein <james.trinklein@changepoint.com>
2016-11-04 16:13:37 -0700

    add projects for consul, feature-flag, update teammember

---
+++ modules/projects/manifests/consul.pp
@@ -0,0 +1,13 @@
+class projects::teammember () {
+  include boxen::config
+
+  host { 'consul':
+    name         => 'devconsul.daptiv.com',
+    comment      => 'dev consul routing',
+    ip           => '127.0.0.1'
+  }
+
+  boxen::project { 'consul':
+    nginx => "${boxen::config::repodir}/modules/projects/templates/consul.nginx.conf.erb"
+  }
+}
---
+++ modules/projects/manifests/feature-flag.pp
@@ -0,0 +1,14 @@
+class projects::teammember () {
+  include boxen::config
+
+  host { 'feature-flag-api':
+    name         => 'devflag.daptiv.com',
+    comment      => 'dev feature flag routing',
+    ip           => '127.0.0.1'
+  }
+
+  boxen::project { 'feature-flag-api':
+    nginx => "${boxen::config::repodir}/modules/projects/templates/feature-flag-api.nginx.conf.erb",
+    source => 'git@github.com:daptiv/feature-flag-api.git'
+  }
+}
